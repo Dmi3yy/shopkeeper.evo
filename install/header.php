946101e2b5969f338bb44163a8673828b5ced79c
Dmi3yy <dmi3yy@gmail.com>
2013-04-29 11:46:12 +0300

    fixed install

--- install/header.php
+++ install/header.php
@@ -1,30 +1,40 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $_lang['language_code']?>" lang="<?php echo $_lang['language_code']?>">
-<head>
-	<title><?php echo $_lang['modx_install']?></title>
-	<meta http-equiv="Content-Type" content="text/html; charset=<?php echo $_lang['encoding']?>" />
-    <link rel="stylesheet" href="style.css" type="text/css" media="screen" />
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+  <head>
+  <title>Shopkeeper <?php echo $_lang['modx_install']?></title>
+  <style type="text/css">
+  body {background-color:#006BAC; padding:10px 15px 15px 15px; margin:0;}
+  body, td {font: 14px Arial, sans-serif;}
+  #content {padding:15px; min-height:450px; background-color:#fff; border-radius: 15px; -o-border-radius:15px; -moz-border-radius: 15px; -webkit-border-radius: 15px; -khtml-border-radius: 15px;}
+  table {background-color: #EFEFEF;}
+  table td, table th {border:1px solid #BFBFBF;}
+  th {background-color:#DFDFDF;}
+  li {margin:10px 0;}
+  h1, h2, h3 {color:#5F5F5F;}
+  </style>
+  
+  	<meta http-equiv="Content-Type" content="text/html; charset=<?php echo $_lang['encoding']?>" />
+    <link rel="stylesheet" href="style.css?v=2" type="text/css" media="screen" />
     <!--[if lt IE 7]>
         <style type="text/css">
             body { behavior: url(/assets/js/csshover3.htc) }
         </style>
         <script type="text/javascript" src="/assets/js/frankensleight.js"></script>
     <![endif]-->
-</head>
 
+  </head>
 <body<?php echo $modx_textdir ? ' id="rtl"':''?>>
-<!-- start install screen-->
-<div id="header">
-    <div class="container_12">
-        <span class="help"><a href="<?php echo $_lang["help_link"] ?>" target="_blank" title="<?php echo $_lang["help_title"] ?>"><?php echo $_lang["help"] ?></a></span>
-		<span class="version"><?php echo $moduleName.' '.$moduleVersion.' ('.($modx_textdir?'&rlm;':'').$modx_release_date?>)</span>
-        <div id="mainheader">
-        	<h1 class="pngfix" id="logo"><span>MODX CMS</span></h1>
-        </div>
-    </div>
-</div>
-<!-- end header -->
 
-<div id="contentarea">
-    <div class="container_12">        
-        <!-- start content -->
+<div style="padding:0 0 10px 0;">
+  <a href="http://modx-shopkeeper.ru/" target="_blank">
+    <img src="img/logo.png" alt="Shopkeeper" border="0" height="50" width="215" />
+  </a>
+</div>
+
+
+<div id="content">
+
+
+<span class="version">Shopkeeper <?php echo $moduleVersion; ?></span>
+    
+
